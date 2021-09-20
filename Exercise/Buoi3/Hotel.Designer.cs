6f245c74e5242203939b44cbdb47aff9d18dcf03
cuong129 <pincontho2612@gmail.com>
2021-09-20 16:45:22 +0700

    done buoi 3

---
+++ Exercise/Buoi3/Hotel.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace Exercise.Buoi3
+{
+    partial class Hotel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hotel));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstBoxDays = new System.Windows.Forms.ListBox();
+            this.rbOccupied = new System.Windows.Forms.RadioButton();
+            this.rbAvailable = new System.Windows.Forms.RadioButton();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(357, 261);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstBoxDays);
+            this.groupBox1.Controls.Add(this.rbOccupied);
+            this.groupBox1.Controls.Add(this.rbAvailable);
+            this.groupBox1.Controls.Add(this.txtPrice);
+            this.groupBox1.Controls.Add(this.cbType);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 261);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin phòng";
+            // 
+            // lstBoxDays
+            // 
+            this.lstBoxDays.Font = new System.Drawing.Font("Arial", 12F);
+            this.lstBoxDays.FormattingEnabled = true;
+            this.lstBoxDays.ItemHeight = 18;
+            this.lstBoxDays.Location = new System.Drawing.Point(215, 221);
+            this.lstBoxDays.Name = "lstBoxDays";
+            this.lstBoxDays.ScrollAlwaysVisible = true;
+            this.lstBoxDays.Size = new System.Drawing.Size(121, 22);
+            this.lstBoxDays.TabIndex = 10;
+            // 
+            // rbOccupied
+            // 
+            this.rbOccupied.AutoSize = true;
+            this.rbOccupied.Location = new System.Drawing.Point(217, 187);
+            this.rbOccupied.Name = "rbOccupied";
+            this.rbOccupied.Size = new System.Drawing.Size(56, 22);
+            this.rbOccupied.TabIndex = 9;
+            this.rbOccupied.Text = "Đầy";
+            this.rbOccupied.UseVisualStyleBackColor = true;
+            // 
+            // rbAvailable
+            // 
+            this.rbAvailable.AutoSize = true;
+            this.rbAvailable.Checked = true;
+            this.rbAvailable.Location = new System.Drawing.Point(217, 159);
+            this.rbAvailable.Name = "rbAvailable";
+            this.rbAvailable.Size = new System.Drawing.Size(76, 22);
+            this.rbAvailable.TabIndex = 8;
+            this.rbAvailable.TabStop = true;
+            this.rbAvailable.Text = "Trống";
+            this.rbAvailable.UseVisualStyleBackColor = true;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(215, 110);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 26);
+            this.txtPrice.TabIndex = 7;
+            this.txtPrice.Text = "150000";
+            // 
+            // cbType
+            // 
+            this.cbType.Font = new System.Drawing.Font("Arial", 12F);
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D"});
+            this.cbType.Location = new System.Drawing.Point(215, 69);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 26);
+            this.cbType.TabIndex = 6;
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(215, 30);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(121, 26);
+            this.txtID.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(17, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số ngày ở :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(17, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tình trạng phòng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(17, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giá phòng :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Loại phòng :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã phòng :";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Location = new System.Drawing.Point(13, 279);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(357, 181);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCheckout);
+            this.groupBox2.Controls.Add(this.btnCancel);
+            this.groupBox2.Controls.Add(this.btnOrder);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(357, 181);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức năng";
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnCheckout.ForeColor = System.Drawing.Color.White;
+            this.btnCheckout.Location = new System.Drawing.Point(206, 136);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(130, 31);
+            this.btnCheckout.TabIndex = 4;
+            this.btnCheckout.Text = "Thanh toán";
+            this.btnCheckout.UseVisualStyleBackColor = false;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(206, 99);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 31);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Huỷ phòng";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnOrder.ForeColor = System.Drawing.Color.White;
+            this.btnOrder.Location = new System.Drawing.Point(206, 62);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(130, 31);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "Đặt phòng";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(206, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 31);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Thêm phòng";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Exercise.Properties.Resources.motel;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(375, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(485, 448);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Room.png");
+            // 
+            // Hotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(872, 465);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Hotel";
+            this.Text = "Hotel";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstBoxDays;
+        private System.Windows.Forms.RadioButton rbOccupied;
+        private System.Windows.Forms.RadioButton rbAvailable;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ Exercise/Buoi3/Hotel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise.Buoi3
+{
+    public partial class Hotel : Form
+    {
+        DataTable dt = new DataTable();
+        private void InitDataTable()
+        {
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("Type", typeof(string));
+            dt.Columns.Add("Price", typeof(double));
+            dt.Columns.Add("Status", typeof(bool)); // 1: Available || 0: Occupied
+            dt.Columns.Add("TotalDays", typeof(int));
+        } 
+        public Hotel()
+        {
+            InitializeComponent();
+            InitDataTable();
+            cbType.SelectedIndex = 0;
+            listView1.LargeImageList = imageList1;
+            //listView1.View = View.LargeIcon;     
+            lstBoxDays.Items.AddRange(new string[] { "5", "4", "3", "2", "1", "0" });
+            lstBoxDays.SelectedIndex = 5;
+        }
+
+        private string GenerateID(string type)
+        {
+            DataRow[] dr = dt.Select("Type='" + type + "'");
+            for(int i = 0; i < dr.Length; i++)
+            {
+                if (dr[i]["ID"].ToString() != type + (i + 1))
+                    return type + (i + 1);            
+            }
+            return type + (dr.Length + 1);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string type = cbType.SelectedItem.ToString();
+            string id = GenerateID(type);
+            double price = Convert.ToDouble(txtPrice.Text);
+            bool status = rbAvailable.Checked;
+            int totalDays = Convert.ToInt32(lstBoxDays.SelectedItem);
+            dt.Rows.Add(id, type, price, status, totalDays);
+            listView1.Items.Add(id, 0);
+        }
+        private void LoadData(string id)
+        {
+            DataRow[] dr = dt.Select("ID='" + id + "'");
+            txtID.Text = dr[0]["ID"].ToString();
+            cbType.Text = dr[0]["Type"].ToString();
+            txtPrice.Text = dr[0]["Price"].ToString();
+            rbAvailable.Checked = Convert.ToBoolean(dr[0]["Status"]);
+            rbOccupied.Checked = !rbAvailable.Checked;
+            lstBoxDays.SelectedIndex = 5 - (int)dr[0]["TotalDays"];
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            LoadData(e.Item.Text);
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            if (rbAvailable.Checked)
+            {
+                DataRow dr = dt.Select("ID='" + txtID.Text + "'").FirstOrDefault();
+                if (dr != null)
+                {
+                    dr["Status"] = false;
+                    dr["TotalDays"] = Convert.ToInt32(lstBoxDays.SelectedItem);
+                }
+                LoadData(txtID.Text);
+            }
+            else
+            {
+                MessageBox.Show("Phòng này đã có người đặt rồi!");
+            }
+        }
+        private void SetEmptyRoom()
+        {
+            DataRow dr = dt.Select("ID='" + txtID.Text + "'").FirstOrDefault();
+            if (dr != null)
+            {
+                dr["Status"] = true;
+                dr["TotalDays"] = 0;
+            }      
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            SetEmptyRoom();
+            LoadData(txtID.Text);
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            DialogResult dlg = MessageBox.Show("Bạn cần thanh toán số tiền là " + txtPrice.Text + " đồng", "Thông báo",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (dlg == DialogResult.OK)
+                SetEmptyRoom();
+            LoadData(txtID.Text);
+        }
+    }
+}
