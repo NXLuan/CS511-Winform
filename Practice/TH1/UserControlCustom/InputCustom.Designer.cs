29f6be755825ffa15db997ba01cb0f9ced508e63
cuong129 <pincontho2612@gmail.com>
2021-10-15 13:25:13 +0700

    1

--- Practice/TH1/UserControlCustom/InputCustom.Designer.cs
+++ Practice/TH1/UserControlCustom/InputCustom.Designer.cs
@@ -44,9 +44,10 @@ namespace Practice.TH1.UserControlCustom
             this.lb_Title.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_Title.ForeColor = System.Drawing.Color.Gray;
             this.lb_Title.Location = new System.Drawing.Point(0, 0);
+            this.lb_Title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_Title.Name = "lb_Title";
-            this.lb_Title.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.lb_Title.Size = new System.Drawing.Size(34, 27);
+            this.lb_Title.Padding = new System.Windows.Forms.Padding(0, 4, 0, 4);
+            this.lb_Title.Size = new System.Drawing.Size(30, 23);
             this.lb_Title.TabIndex = 0;
             this.lb_Title.Text = "Title";
             // 
@@ -55,10 +56,11 @@ namespace Practice.TH1.UserControlCustom
             this.panel1.Controls.Add(this.tb_Content);
             this.panel1.Controls.Add(this.pn_Underline);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Location = new System.Drawing.Point(0, 23);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
-            this.panel1.Size = new System.Drawing.Size(303, 29);
+            this.panel1.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(227, 23);
             this.panel1.TabIndex = 1;
             // 
             // tb_Content
@@ -66,9 +68,10 @@ namespace Practice.TH1.UserControlCustom
             this.tb_Content.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tb_Content.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tb_Content.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_Content.Location = new System.Drawing.Point(4, 0);
+            this.tb_Content.Location = new System.Drawing.Point(3, 0);
+            this.tb_Content.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_Content.Name = "tb_Content";
-            this.tb_Content.Size = new System.Drawing.Size(299, 20);
+            this.tb_Content.Size = new System.Drawing.Size(224, 16);
             this.tb_Content.TabIndex = 1;
             this.tb_Content.TextChanged += new System.EventHandler(this.tb_Content_TextChanged);
             this.tb_Content.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_Content_KeyPress);
@@ -77,20 +80,22 @@ namespace Practice.TH1.UserControlCustom
             // 
             this.pn_Underline.BackColor = System.Drawing.Color.Silver;
             this.pn_Underline.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pn_Underline.Location = new System.Drawing.Point(4, 26);
+            this.pn_Underline.Location = new System.Drawing.Point(3, 21);
+            this.pn_Underline.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pn_Underline.Name = "pn_Underline";
-            this.pn_Underline.Size = new System.Drawing.Size(299, 3);
+            this.pn_Underline.Size = new System.Drawing.Size(224, 2);
             this.pn_Underline.TabIndex = 0;
             // 
             // InputCustom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lb_Title);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InputCustom";
-            this.Size = new System.Drawing.Size(303, 56);
+            this.Size = new System.Drawing.Size(227, 46);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
